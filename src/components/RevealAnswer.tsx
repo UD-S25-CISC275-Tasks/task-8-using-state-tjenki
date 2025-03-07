5e1ac9393f8ec1a7973465dff1bf8508dae6c7fd
Taylor Jenkins <tjenkins@udel.edu>
2025-03-06 21:34:20 -0500

    attempted reveal answer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,17 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    let [visible, setVisible] = useState<boolean>(true);
+    function answervisible(): void {
+        setVisible(!visible);
+    }
+    return (
+        <div>
+            Reveal Answer
+            <span>
+                <Button onClick={answervisible}>Reveal Answer</Button>
+                {visible && <div>42</div>}
+            </span>
+        </div>
+    );
 }
