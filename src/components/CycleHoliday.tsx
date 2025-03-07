24f3e2c002906482eb2152847fa2accd38c7b8b0
Taylor Jenkins <tjenkins@udel.edu>
2025-03-07 16:55:00 -0500

    all components work but some tests do not pass

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -2,5 +2,41 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function CycleHoliday(): React.JSX.Element {
-    return <div>Cycle Holiday</div>;
+    type Holiday =
+        | "Valentines Day: 🩷"
+        | "Memorial Day: 👩🏿‍✈️"
+        | "Easter: 🐰"
+        | "Christmas: 🎅🏿"
+        | "Juneteenth: 💃🏿";
+    let HOLIDAY_TIME_TRANSITIONS: Record<Holiday, Holiday> = {
+        "Valentines Day: 🩷": "Easter: 🐰",
+        "Easter: 🐰": "Memorial Day: 👩🏿‍✈️",
+        "Memorial Day: 👩🏿‍✈️": "Juneteenth: 💃🏿",
+        "Juneteenth: 💃🏿": "Christmas: 🎅🏿",
+        "Christmas: 🎅🏿": "Valentines Day: 🩷"
+    };
+    let HOLIDAY_ALPHABET_TRANSITIONS: Record<Holiday, Holiday> = {
+        "Christmas: 🎅🏿": "Easter: 🐰",
+        "Easter: 🐰": "Juneteenth: 💃🏿",
+        "Juneteenth: 💃🏿": "Memorial Day: 👩🏿‍✈️",
+        "Memorial Day: 👩🏿‍✈️": "Valentines Day: 🩷",
+        "Valentines Day: 🩷": "Christmas: 🎅🏿"
+    };
+    let [holiday, setHoliday] = useState<Holiday>("Juneteenth: 💃🏿");
+
+    function advanceAlphabetically(): void {
+        let nextHoliday = HOLIDAY_ALPHABET_TRANSITIONS[holiday];
+        setHoliday(nextHoliday);
+    }
+    function advanceByYear(): void {
+        let nextHoliday = HOLIDAY_TIME_TRANSITIONS[holiday];
+        setHoliday(nextHoliday);
+    }
+    return (
+        <div>
+            <span>{holiday}</span>
+            <Button onClick={advanceAlphabetically}>Advance By Alphabet</Button>
+            <Button onClick={advanceByYear}>Advance By Year</Button>
+        </div>
+    );
 }
